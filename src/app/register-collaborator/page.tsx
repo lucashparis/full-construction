bd089ded3e5a04b5a6664eb8c3fef5ecf62c9baa
Lucas Paris <lucashenriqueparis@gmail.com>
2023-05-01 22:36:36 -0300

    Add register collaborator page

--- src/app/register-collaborator/page.tsx
+++ src/app/register-collaborator/page.tsx
@@ -1,6 +1,9 @@
 'use client';
 import Collaborator from '@/class/Collaborator';
-import BasicAlerts from '@/component/Alert';
+import BasicAlerts from '@/components/Alert';
+import Container from '@/components/Container';
+import Header from '@/components/Header';
+import MyMenu from '@/components/MyMenu';
 import { TextField, Button } from '@mui/material';
 import { useState } from 'react';
 
@@ -8,31 +11,35 @@ export default function RegisterCollaborator() {
 
     const [name, setName] = useState('');
     const [daily, setDaily] = useState(0);
-    const [errorMessage, setErrorMessage] = useState('');
+    const [alertMessage, setAlertMessage] = useState({error: false, message: ''});
 
     const createCollaborator = (event:any) => {
         event.preventDefault();
 
         try {
             const collaborator = new Collaborator(name, daily);
-            console.log(collaborator);
+            setAlertMessage({error: false, message: 'Funcionario cadastrado!'});
         } catch (error) {
-            setErrorMessage(error);
-            console.log(error);
+            setAlertMessage({error: true, message: String(error)});
         }
     }
 
     return (
         <>
-            { errorMessage && (
-                <BasicAlerts message={errorMessage} />
-            )}
-            <h1> Register Collaborator </h1>
-            <form onSubmit={createCollaborator}>
-                <TextField id="standard-basic" label="Nome" variant="standard" onChange={() => {setName(event.target.value)}} />
-                <TextField id="standard-basic" type="number" label="Diária" variant="standard" onChange={()=> {setDaily(event.target.value)}} />
-                <Button className='bg-[#1976d2]' type="submit" variant="contained"> Cadastrar </Button>
-            </form>
+            <MyMenu/>
+            <Header description={"Cadastro de Funcionário"}/>
+            <Container>
+                { alertMessage.message && (
+                    <BasicAlerts error={alertMessage.error} message={alertMessage.message} />
+                )}
+                <form onSubmit={createCollaborator}>
+                    <div className='mb-4'>
+                        <TextField className='w-full' id="standard-basic" label="Nome" variant="standard" onChange={() => {setName(event.target.value)}} />
+                        <TextField className='w-full' id="standard-basic" type="number" label="Diária" variant="standard" onChange={()=> {setDaily(event.target.value)}} />
+                    </div>
+                    <Button className='button' type="submit" variant="contained"> Cadastrar </Button>
+                </form>
+            </Container>
         </>
     );
 }
