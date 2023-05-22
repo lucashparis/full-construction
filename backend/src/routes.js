1eb29247431b3f5c97d203d7a4a7410129ed44aa
Lucas Paris <lucashenriqueparis@gmail.com>
2023-05-22 20:52:06 -0300

    Implement passing id by parameter in api route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,8 @@ import CollaboratorController from './controllers/CollaboratorController';
 const routes = Router();
 
 routes.get('/api/collaborators', CollaboratorController.getCollaboratorsController);
+routes.get('/api/collaborator/:id', CollaboratorController.getCollaboratorController);
 routes.post('/api/create-collaborator', CollaboratorController.createCollaboratorController);
+routes.put('/api/update-collaborator', CollaboratorController.updateCollaboratorController);
 
 export default routes;
